5051cd30964185116e916432abb48d87df6531e3
Vishnu <vishnu.kraman89.outlook.com>
2018-08-26 22:52:35 +0530

    Adding BHK option

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -135,13 +135,26 @@
                     </div>
 
                     <div class="col">
-                        <select name="for" id="forgender" class="form-control from-control-lg">
+                            <div id="ifHome1" style="display:block;">
+                                    <label for="forgender">Property for</label>
+                                    <select name="for" id="forgender" class="form-control from-control-lg">
+                                        <option value="male">Male</option>
+                                        <option value="female">Female</option>
+                                        <option value="both">Both</option>
+                                        <option value="others">Others</option>
+                                    </select>
+                                </div>
 
-                            <option value="male">Male</option>
-                            <option value="female">Female</option>
-                            <option value="both">Both</option>
-                            <option value="others">Others</option>
-                        </select>
+                                <div id="ifHome" style="display:none;">
+                                    <label for="bhk">Select </label>
+                                    <select name="bhk" id="forgender" class="form-control from-control-lg">
+                                        <option value="1 BHk">1 BHK</option>
+                                        <option value="2 BHK">2 BHK</option>
+                                        <option value="3 BHK">3 BHK</option>
+                                        <option value="3+ BHK">3+ BHK</option>
+                                    </select>
+                                </div>
+                            </div>
                     </div>
                 </div>
 
@@ -154,8 +167,18 @@
         </div>
     </section>
 
+<script>
+    function yesnoCheck(that) {
+        if (that.value == "home") {
+            document.getElementById("ifHome").style.display = "block";
+            document.getElementById("ifHome1").style.display = "none";
 
-
+        } else {
+            document.getElementById("ifHome").style.display = "none";
+            document.getElementById("ifHome1").style.display = "block";
+        }
+    }
+</script>
 
 <!--  </body> -->
 
