fab847dc4c214c5807275b9c785d6f1a54bf6ad7
anupam shukla <shuklaanupam18@gmail.com>
2018-08-29 23:52:37 +0530

    Improved UI v1.1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -50,7 +50,7 @@
 <div class="container text-center">
     <div class="row p-5">
         <div class="col-md-12">
-            <img src="Images/down.svg" alt="" style="height:80px ; width:100px;" onClick="document.getElementById('down-arrow').scrollIntoView();">
+            <img src="Images/down.svg" alt="" style="height:80px ; width:100px;" onClick="document.getElementById('down-arrow-1').scrollIntoView();">
         </div>
     </div>
 </div>
@@ -59,7 +59,7 @@
 
 <div class="row pt-5" id="down-arrow-1">
     <div class="col-md-6">
-        <p class="display-4 text-center">Looking for<br> <b class="highlight-text" style="color:rgb(185, 60, 64);"> a PG</b>
+        <p class="display-4 text-right">Looking for<br> <b class="highlight-text" style="color:rgb(185, 60, 64);"> a PG</b>
             <br>or<b class="high-text" style="color:rgb(185, 60, 64);">
                 Home?</b></p>
     </div>
@@ -67,23 +67,23 @@
     </div>
 </div>
 
-<div class="row" id="down-arrow-2">
+<div class="row d-flex align-content-center" id="down-arrow-2">
     <div class="col-md-6">
     </div>
-    <div class="col-md-6">
-        <p class="display-4 text-center">We are here to <br> <b class="highlight-text" style="color:rgb(185, 60, 64);">Help
+    <div class="col-md-6 left">
+        <p class="display-4">We are here to <br> <b class="highlight-text" style="color:rgb(185, 60, 64);">Help
                 You.</p>
     </div>
 </div>
 
 
-<div class="row details p-5 description">
+<div class="row p-5 description">
     <div class="col-md-3">
-        <h1>Everything you need.</h1>
+        <h1><b style="color:rgb(185, 60, 64);">Everything</b> you need.</h1>
     </div>
     <div class="col-md-3" style="font-family:'Lato'; ">
         <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="/Images/home.svg" alt="Card image cap">
+            <img class="card-img-top" src="/Images/home.svg" alt="Card image cap">
             <div class="card-body">
                 <h5 class="card-title">Single House</h5>
                 <h6 class="card-subtitle mb-2 text-muted">For Family</h6>
@@ -96,7 +96,7 @@
 
     <div class="col-md-3" style="font-family:'Lato'; ">
         <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="/Images/home1.svg" alt="Card image cap">
+            <img class="card-img-top" src="/Images/home1.svg" alt="Card image cap">
             <div class="card-body">
                 <h5 class="card-title">Single Sharing PG</h5>
                 <h6 class="card-subtitle mb-2 text-muted">For Students/Employees</h6>
@@ -109,7 +109,7 @@
 
     <div class="col-md-3" style="font-family:'Lato'; ">
         <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="/Images/home2.svg" alt="Card image cap">
+            <img class="card-img-top" src="/Images/home2.svg" alt="Card image cap">
             <div class="card-body">
                 <h5 class="card-title">Multiple Sharing PG</h5>
                 <h6 class="card-subtitle mb-2 text-muted">For Students/Employees</h6>
@@ -124,11 +124,11 @@
 
 <div class="row p-5 search-steps">
     <div class="col-md-4">
-        <h1>Simplified Search.</h1>
+        <h1>Simplified<b style="color:rgb(185, 60, 64);"> Search</b></h1>
     </div>
     <div class="col-md-4" style="font-family:'Lato'; ">
         <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="/Images/one.svg" alt="Card image cap">
+            <img class="card-img-top" src="/Images/one.svg" alt="Card image cap">
             <div class="card-body">
                 <h5 class="card-title">Type in the search fields.</h5>
                 <h6 class="card-subtitle mb-2 text-muted">All the fields are required.</h6>
@@ -141,21 +141,24 @@
 
     <div class="col-md-4" style="font-family:'Lato'; ">
         <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="/Images/two.svg" alt="Card image cap">
+            <img class="card-img-top" src="/Images/two.svg" alt="Card image cap">
             <div class="card-body">
                 <h5 class="card-title">Hit Search.</h5>
                 <h6 class="card-subtitle mb-2 text-muted">Please wait for 5 seconds.</h6>
-                <p class="card-text">Search results will be displayed.</p>
+                <p class="card-text">Search results will be displayed based on your preferences.</p>
                 <!-- <a href="#" class="card-link">Card link</a>
                 <a href="#" class="card-link">Another link</a> -->
             </div>
         </div>
     </div>
-
-   
 </div>
 
-
+<div class="row are-you text-center py-5">
+    <div class="col-md-12">
+        <h1>Are you a<b style="color:rgb(185, 60, 64);"> Owner?</b></h1>
+        <button type="button" class="btn btn-primary btn-lg btn-c my-5">Click Here</button>
+    </div>
+</div>
 
 
 
